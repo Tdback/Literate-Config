f691a6f73bb0c3a0565e5cfe82e7c113cdb3e67a
Tyler <tylerdback.etc@proton.me>
2023-03-30 00:44:14 -0400

    Added functions to clean up directories

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -42,6 +42,14 @@
   (unless (server-running-p)
     (server-start)))
 
+(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory))))
+
+;; auto-save-mode doesn't create the path automatically!
+(make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
+
+(setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
+      auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/auto-saves/" user-emacs-directory) t)))
+
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)		; Disable visible scrollbar 
