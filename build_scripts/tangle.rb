bb68d098ea34f980c11e7585a36b4f9ee65e1235
Tyler <tylerdback.etc@proton.me>
2023-06-22 00:09:14 -0400

    Fixed a small issue with directories list

--- build_scripts/tangle.rb
+++ build_scripts/tangle.rb
@@ -3,7 +3,7 @@
 # ===============================================================================
 # 
 #   Author  : Tyler Dunneback
-#   Date    : Jun 15, 2023
+#   Date    : Jun 22, 2023
 #   GitHub  : https://github.com/Tdback
 #   Version : 1.0
 # 
@@ -13,17 +13,17 @@
 require 'fileutils'
 
 directories = [
-  File.expand_path "~/.config/",
-  File.expand_path "~/.config/alacritty/",
-  File.expand_path "~/.config/autostart/",
-  File.expand_path "~/.config/bspwm/",
-  File.expand_path "~/.config/sxhkd/",
-  File.expand_path "~/.config/polybar/",
-  File.expand_path "~/.config/rofi/"
+  File.expand_path("~/.config/"),
+  File.expand_path("~/.config/alacritty/"),
+  File.expand_path("~/.config/autostart/"),
+  File.expand_path("~/.config/bspwm/"),
+  File.expand_path("~/.config/sxhkd/"),
+  File.expand_path("~/.config/polybar/"),
+  File.expand_path("~/.config/rofi/")
 ]
 
 directories.each { |path|
-  unless Dir.exist?(path)
+  unless Dir.exist? path 
     puts "Directory '#{path}' not found, creating it now..."
     Dir.mkdir path
   end
@@ -36,4 +36,4 @@ Dir.mkdir ".temp/"
 system "emacs -Q --script tangle_helper.el"
 
 puts "Cleaning up dependencies and temporary directory..."
-FileUtils.rm_r ".temp/", :force => true, :secure => true
+FileUtils.rm_rf ".temp/", :secure => true
