3bf143c3f91093dc9802feee4fdf7a930f52f2b9
Tyler <tylerdback.etc@proton.me>
2023-06-25 21:29:47 -0400

    Added starship org documentation to tangle list

--- build_scripts/tangle_helper.el
+++ build_scripts/tangle_helper.el
@@ -23,7 +23,7 @@
 
 (defcustom dotfiles-org-files '("Alacritty.org" "Autostart.org" "Bspwm.org"
                                 "Emacs.org" "Lockscreen.org" "Sxhkd.org"
-                                "Polybar.org" "Rofi.org")
+                                "Polybar.org" "Rofi.org" "Starship.org")
   "This list of org-files under the `dotfiles-folder' which contain configuration
 files that should be tangled."
   :type '(list string)
