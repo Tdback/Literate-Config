48c42fee95650a731970298cf46b9ba4cfc7df3f
Tyler <tylerdback.etc@proton.me>
2023-07-14 23:21:37 -0400

    Set script to echo out command before executing.

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -xe
+
 # Check if Script is Run as Root
 if [[ $EUID -ne 0 ]]; then
   echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
